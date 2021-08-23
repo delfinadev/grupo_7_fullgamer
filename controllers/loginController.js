943e595c2737933841392bb682cf0706c254ab60
annaluzserena <annaluzserena@protonmail.com>
2021-08-23 15:35:29 -0300

    Formulario edición

--- controllers/loginController.js
+++
@@ -1,7 +0,0 @@
-const controller = {
-    index: (req, res) => {
-        res.render("login");
-    }
-};
-
-module.exports = controller
--- routes/product.js
+++ routes/product.js
@@ -8,4 +8,6 @@ router.get("/create", productController.create);
 
 router.get("/detail/:id", productController.detail);
 
+router.get("/:id/edit", productController.edit);
+
 module.exports = router
