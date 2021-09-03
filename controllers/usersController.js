d3699a7384fa953dfbbef185cc617a055e125575
annaluzserena <annaluzserena@protonmail.com>
2021-09-02 21:33:39 -0300

    Register validations

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,37 @@
+const path = require("path");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
+
+let readUsers = fs.readFileSync(path.resolve(__dirname, "../data/usuarios.json"));
+let users = JSON.parse(readUsers);
+
+const { validationResult } = require("express-validator");
+
 const controller = {
     register: (req, res) => {
-        res.render("register");
+        res.render("register", {errorMessages: null}, {old: null});
     },
     login: (req, res) => {
         res.render("login");
+    },
+    store: (req, res) => {
+        let errores = validationResult(req);
+        if(!errores.isEmpty()) {
+            res.render("register", {errorMessages: errores.mapped()}, {old: req.body});
+        } else {
+            let newUser = {
+                user: req.body.name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password1, 10),
+                notifications: req.body.notificaciones !== undefined ? "on" : "off",
+                image: req.file ? req.file : "user-default.jpg"
+            };
+            users.push(newUser);
+            JSON.stringify(users, null, 4);
+            fs.writeFileSync(path.resolve(__dirname, "../data/usuarios.json"), users);
+
+            res.redirect("/");
+        }
     }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
 const usersController = require("../controllers/usersController");
+const registerValidations = require("../middlewares/registerValidations");
 
 router.get("/register", usersController.register);
 router.get("/login", usersController.login);
+router.post("/register", registerValidations, usersController.store);
 
 module.exports = router
