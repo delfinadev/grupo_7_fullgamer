f801854477c1009ec0486d87ebf094cfb23ad6a1
annaluzserena <annaluzserena@protonmail.com>
2021-09-03 21:50:32 -0300

    Creación de usuarios sin img

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,37 +2,40 @@ const path = require("path");
 const fs = require("fs");
 const bcrypt = require("bcryptjs");
 
-let readUsers = fs.readFileSync(path.resolve(__dirname, "../data/usuarios.json"));
-let users = JSON.parse(readUsers);
+let readUsers = fs.readFileSync(path.resolve(__dirname, "../data/usuarios.json"), {encoded: "utf-8"});
+let users = JSON.parse(readUsers, null, 4);
 
 const { validationResult } = require("express-validator");
 
 const controller = {
     register: (req, res) => {
-        res.render("register", {errorMessages: null}, {old: null});
+        res.render("register", {errorMessages: null, old: null});
     },
     login: (req, res) => {
         res.render("login");
     },
     store: (req, res) => {
         let errores = validationResult(req);
+
         if(!errores.isEmpty()) {
-            res.render("register", {errorMessages: errores.mapped()}, {old: req.body});
+            console.log(errores.mapped());
+            res.render("register", {errorMessages: errores.mapped(), old: req.body});
         } else {
             let newUser = {
-                user: req.body.name,
-                email: req.body.email,
-                password: bcrypt.hashSync(req.body.password1, 10),
-                notifications: req.body.notificaciones !== undefined ? "on" : "off",
-                image: req.file ? req.file : "user-default.jpg"
+                "user": req.body.name,
+                "email": req.body.email,
+                "password": bcrypt.hashSync(req.body.password1, 10),
+                "notifications": req.body.notificaciones !== undefined ? "on" : "off",
+                "image": req.file ? req.file.filename : "user-default.jpg"
             };
+
             users.push(newUser);
-            JSON.stringify(users, null, 4);
-            fs.writeFileSync(path.resolve(__dirname, "../data/usuarios.json"), users);
+            let newUsers = JSON.stringify(users, null, 4);
+            fs.writeFileSync(path.resolve(__dirname, "../data/usuarios.json"), newUsers);
 
             res.redirect("/");
-        }
+        };
     }
-}
+};
 
-module.exports = controller
+module.exports = controller;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
 
 const usersController = require("../controllers/usersController");
 const registerValidations = require("../middlewares/registerValidations");
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/img/users");
+    },
+    filename: function (req, file, cb) {
+        cb(null, req.file + "-" + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
 router.get("/register", usersController.register);
+router.post("/register", registerValidations, upload.single("image"), usersController.store);
+
 router.get("/login", usersController.login);
-router.post("/register", registerValidations, usersController.store);
+
 
 module.exports = router
