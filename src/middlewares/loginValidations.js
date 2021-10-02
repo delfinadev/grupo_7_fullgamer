215f4386ecc30503a38bfb90e34a9c232d85c1e9
unknown <delfinalasserre@gmail.com>
2021-10-02 11:04:30 -0300

    fix loginValidations

--- src/middlewares/loginValidations.js
+++ src/middlewares/loginValidations.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const { text } = require("express");
-const { hash } = require("bcryptjs");
+const { bycrypt } = require("bcryptjs");
 const { check } = require("express-validator");
 
 let readUsers = fs.readFileSync(path.resolve(__dirname, "../data/usuarios.json"), {encoded: "utf-8"});
@@ -14,21 +14,20 @@ let users = JSON.parse(readUsers, null, 4);
 const loginValidations = [
     check('email')
     .custom(async (email) => {
-        if(users[i].email != email) {
+        for(let i = 0; i < users.length; i++)
+            if(users[i].email == email) {
+                let indexUsuario = i;
+        }
+        if (indexUsuario == undefined){
             throw new error("El email no está registrado")
         }
     }).bail(),
 
 
     check('password')
-    // .isEmail().withMessage('Email inválido')
-    // .isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
-    // .custom(bycrypt.compareSync (password, hash)) => { {
-    //     if(users[i].email == email) 
-// no se de donde saque esta parte del if para el email pero no me cierra
-.custom(async (password) => {
-    const password = req.body.password,
-    if(password !== confirmPassword){
-        throw new Error('Contraseña inválida. Intente de nuevo.')}
+        .custom(async (password) => {
+        const password = req.body.password,
+            if(bcrypt.compareSync(password, users[indexUsuario]. password)){
+                throw new Error('Contraseña inválida. Intente de nuevo.')}
     }),
 ]
