cd32509387669d9a4300f7a15d5dff0fa77ae5dd
annaluzserena <annaluzserena@protonmail.com>
2021-10-25 19:36:01 -0300

    Store y destroy

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,44 +5,39 @@ let db = require("../../database/models");
 
 const controller = {
     index: (req, res) => {
-        res.render("listadoProductos", { products: products });
+        db.Productos.findAll()
+            .then(function(products) {
+                res.render("listadoProductos", { products: products });
+            });
     },
     create: (req, res) => {
         res.render("agregarProducto", { product: null });
     },
     detail: (req, res) => {
         let id = req.params.id;
-        res.render("producto", { product: products[id] });
+        db.Productos.findByPk(id)
+            .then(function(product) {
+                res.render("producto", { product: product });
+            });
     },
     edit: (req, res) => {
         let id = req.params.id;
-        res.render("agregarProducto", { product: products[id] });
+        db.Productos.findByPk(id)
+            .then(function(product) {
+                res.render("agregarProducto", { product: product });
+            });
     },
     store: (req, res) => {
         db.Productos.create({
             name: req.body.name,
             price: req.body.price, 
-            description: req.body.description
+            description: req.body.description,
+            category_id: req.body.category,
+            created_at: Date.now()
         });
-
-        let newProduct = {
-            "id": products.length,
-            "name": req.body.name,
-            "description": req.body.description,
-            "image": req.file.filename,
-            "image2": "mouse-segunda-foto.jpg",
-            "category": req.body.category,
-            "price": req.body.price,
-            "date": req.body.date
-        };
-
-        products.push(newProduct);
-        let newProducts = JSON.stringify(products, null, 4);
-        fs.writeFileSync(path.resolve(__dirname, "../data/products.json"), newProducts);
 
         res.redirect("/products");
     },
-    // Editar un producto en el json
     update: (req, res) => {
 
         let id = req.params.id;
@@ -61,21 +56,15 @@ const controller = {
 
         res.redirect("/products");
     },
-    //Eliminar un producto del json
     destroy: (req, res) => {
     db.Productos.destroy({
         where: {
             id: req.params.id
         }
     })
-
-        let id = req.params.id;
-
-        products.splice(id, 1);
-        let newProducts = JSON.stringify(products, null, 4);
-        fs.writeFileSync(path.resolve(__dirname, "../data/products.json"), newProducts);
-
-        res.redirect("/products");
+        .then(() => {
+            res.redirect("/products");
+        })
     },
     cart: (req, res) => {
         if(req.session) {
