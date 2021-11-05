7160b1ab4d98051ae445d849bf9c2901d7a09f59
annaluzserena <annaluzserena@protonmail.com>
2021-11-05 14:25:24 -0300

    Habilitar y deshabilitar inputs

---
+++ public/js/editUser.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function() {
+    let inputs = document.querySelectorAll(".input");
+    let pencils = document.querySelectorAll(".pencil");
+
+    for(let i = 0; i < pencils.length; i++) {
+        pencils[i].addEventListener("click", function() {
+            pencils[i].classList.toggle("fa-pen");
+            pencils[i].classList.toggle("fa-check");
+
+            if(inputs[i].disabled === true) {
+                inputs[i].disabled = false;
+                inputs[i].style.backgroundColor = "#323232";
+            } else {
+                inputs[i].disabled = true;
+                inputs[i].style.backgroundColor = "rgb(83, 83, 83)";
+            };
+            
+        });
+    }
+})
