9cb2fa093d82fc01dc706236351204cfa17be79c
ogna1 <mundoigna@gmail.com>
2021-12-12 02:24:53 -0300

    Update App.js

--- src/dashboard/src/App.js
+++ src/dashboard/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import LastProduct from "./components/LastProduct";
+import totalCategories from './components/totalCategories';
 
 function App() {
   return (
     <div className="App">
       <LastProduct/>
     </div>
-  );
+  )
+}
+
+function categorias() {
+  return (
+    <div className="App">
+      <totalCategories/>
+    </div>
+  )
 }
 
 export default App;
